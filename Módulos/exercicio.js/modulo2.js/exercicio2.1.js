4defcc39a0d09597806b8f1b6153c8802d600beb
Jhon Claiton da Luz <jhon_luz@estudante.sesisenai.org.br>
2026-07-08 14:41:30 -0300

    exercicio 1
    modulo 2

---
+++ Módulos/exercicio.js/modulo2.js/exercicio2.1.js
@@ -0,0 +1,23 @@
+let numero = 7;
+
+if (numero % 2 === 0) {
+  console.log(`${numero} é par`);
+} else {
+  console.log(`${numero} é ímpar`);
+}
+
+numero = 4;
+
+if (numero % 2 === 0) {
+  console.log(`${numero} é par`);
+} else {
+  console.log(`${numero} é ímpar`);
+}
+
+numero = 15;
+
+if (numero % 2 === 0) {
+  console.log(`${numero} é par`);
+} else {
+  console.log(`${numero} é ímpar`);
+}
