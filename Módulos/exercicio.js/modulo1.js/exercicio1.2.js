02a1257c44a30c086be42aea05d85cddf62e9211
Jhon Claiton da Luz <jhon_luz@estudante.sesisenai.org.br>
2026-07-08 14:39:32 -0300

    exercício 2
    bloco 1

---
+++ Módulos/exercicio.js/modulo1.js/exercicio1.2.js
@@ -0,0 +1,11 @@
+let numeroInteiro = 10;
+let numeroDecimal = 7.5;
+let nome = "Seu Nome";
+let verdadeiro = true;
+let semValor;
+
+console.log(typeof numeroInteiro);
+console.log(typeof numeroDecimal);
+console.log(typeof nome);
+console.log(typeof verdadeiro);
+console.log(typeof semValor);
