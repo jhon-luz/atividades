926ae6a4395f2ff997aaaa82ce65f714c5212412
Jhon Claiton da Luz <jhon_luz@estudante.sesisenai.org.br>
2026-07-08 14:49:57 -0300

    exercício 2
    bloco 2

---
+++ Módulos/exercicio.js/modulo2.js/exercicio2.2.js
@@ -0,0 +1,47 @@
+let nota = 9.5;
+
+if (nota >= 9) {
+  console.log(`${nota} - Excelente`);
+} else if (nota >= 7) {
+  console.log(`${nota} - Aprovado`);
+} else if (nota >= 5) {
+  console.log(`${nota} - Recuperação`);
+} else {
+  console.log(`${nota} - Reprovado`);
+}
+
+nota = 7.5;
+
+if (nota >= 9) {
+  console.log(`${nota} - Excelente`);
+} else if (nota >= 7) {
+  console.log(`${nota} - Aprovado`);
+} else if (nota >= 5) {
+  console.log(`${nota} - Recuperação`);
+} else {
+  console.log(`${nota} - Reprovado`);
+}
+
+nota = 5.5;
+
+if (nota >= 9) {
+  console.log(`${nota} - Excelente`);
+} else if (nota >= 7) {
+  console.log(`${nota} - Aprovado`);
+} else if (nota >= 5) {
+  console.log(`${nota} - Recuperação`);
+} else {
+  console.log(`${nota} - Reprovado`);
+}
+
+nota = 3;
+
+if (nota >= 9) {
+  console.log(`${nota} - Excelente`);
+} else if (nota >= 7) {
+  console.log(`${nota} - Aprovado`);
+} else if (nota >= 5) {
+  console.log(`${nota} - Recuperação`);
+} else {
+  console.log(`${nota} - Reprovado`);
+}
