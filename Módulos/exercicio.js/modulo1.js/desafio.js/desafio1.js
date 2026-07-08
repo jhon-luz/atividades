8c10a15d2265ad905be3e8742321d936e1f0e64c
Jhon Claiton da Luz <jhon_luz@estudante.sesisenai.org.br>
2026-07-08 14:41:09 -0300

    desafio 1
    modulo 1

---
+++ Módulos/exercicio.js/modulo1.js/desafio.js/desafio1.js
@@ -0,0 +1,10 @@
+const nome = "BRECKS";
+const classe = "Guerreiro";
+let nivel = 197;
+let pontosDeVida = 286;
+let estaVivo = true;
+let armaEquipada = "cavander";
+
+console.log(
+  `${nome} é um(a) ${classe} nível ${nivel} com ${pontosDeVida} HP. Arma: ${armaEquipada}. Status: ${estaVivo ? "vivo" : "morto"}`
+);
